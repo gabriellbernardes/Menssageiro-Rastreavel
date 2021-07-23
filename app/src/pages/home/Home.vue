aa8da9a7cdaba5d349e9c04ec8cc6023f931ea88
Gabriel Bernardes <gabrielbernardes@alu.ufc.br>
2021-07-23 09:10:32 -0300

    subindo

---
+++ app/src/pages/home/Home.vue
@@ -0,0 +1,6 @@
+<script src="https://unpkg.com/axios/dist/axios.min.js"/>
+<script src="./home.js" />
+<template src="./home.html"/>
+<style src="./home.scss" scoped lang="scss">
+  @import 'style';
+</style>
